ded3ed1ab707c72dab7d30c7fef493f175d6e92f
Prince Kumar <princek@terawe.com>
2024-07-23 11:33:42 +0530

    routing changes

--- server/routes/api/index.js
+++ server/routes/api/index.js
@@ -7,10 +7,10 @@ const postUser = require('./postUser');
 const putUser = require('./putUser');
 const validateUser = require('./validateUser');
 
-router.use('/', getUsers);
+router.use('/getAllUsers', getUsers);
 router.use('/', getUserById);
-router.use('/', postUser);
-router.use('/', putUser);
-router.use('/', validateUser);
+router.use('/createUser', postUser);
+router.use('/updateUser', putUser);
+router.use('/validate', validateUser);
 
 module.exports = router;
