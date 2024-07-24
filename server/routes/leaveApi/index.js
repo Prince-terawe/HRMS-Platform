641d7a7606f78efad7364b30f30820485bfc2270
Prince Kumar <princek@terawe.com>
2024-07-24 10:34:49 +0530

    cancelrequest

--- server/routes/leaveApi/index.js
+++ server/routes/leaveApi/index.js
@@ -2,7 +2,9 @@ const express = require('express');
 const router = express.Router();
 
 const leaveRequest = require('./leaveRequest')
+const cancelRequest = require('./cancelRequest');
 
-router.use('/', leaveRequest);
+router.use('/leaveRequest', leaveRequest);
+router.use('/cancelRequest', cancelRequest);
 
 module.exports = router;
